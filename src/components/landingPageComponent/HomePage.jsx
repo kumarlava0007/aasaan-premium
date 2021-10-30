f071c8a958e85b53ee0fd38e34b9d43d1491c783
kumarlava0007 <kumarlava0007@gmail.com>
2021-10-30 21:23:03 +0530

    update 3

--- src/components/landingPageComponent/HomePage.jsx
+++ src/components/landingPageComponent/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import FirstSectionComponent from './FirstSectionComponent'
 import SecondSectionComponent from './SecondSectionComponent'
+import ThirdSectionComponent from './ThirdSectionComponent'
 const HomePage = () => {
     return (
         <div>
             <FirstSectionComponent/>
             <SecondSectionComponent/>
+            <ThirdSectionComponent />
         </div>
     )
 }
