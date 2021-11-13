1249b4ca392c015bc367aa5737aa264b3d4213e2
kumarlava0007 <kumarlava0007@gmail.com>
2021-11-13 16:29:39 +0530

    cards update

--- src/components/landingPageComponent/FirstSectionComponent.jsx
+++ src/components/landingPageComponent/FirstSectionComponent.jsx
@@ -22,25 +22,83 @@ export default function FirstSectionComponent() {
                     <i class="far fa-handshake fa-7x first-section-img"></i>
                     <div>
                     <h4>A commitment <br /> to our customers</h4>
-                    <button className="linksTwo">Know more</button>
+                    <button className="second-button">Know more</button>
                     </div>
                 </div>
             </div>
             <div className="firstSection-main-two">
                 <div className="firstSection-main-two-container">
                     <div className="two-container-cards">
-                        <div className="cards-card"></div>
-                        <div className="cards-card"></div>
-                        <div className="cards-card"></div>
-                        <div className="cards-card"></div>
-                        <div className="cards-card"></div>
-                        <div className="cards-card"></div>
-                        <div className="cards-card"></div>
-                        <div className="cards-card"></div>
-                        <div className="cards-card"></div>
-                        <div className="cards-card"></div>
-                        <div className="cards-card"></div>
-                        <div className="cards-card"></div>
+                        <div className="cards-card">
+                        <i class="fas fa-umbrella cardsIcons"></i>
+                        <p>Term Life <br />Insurance</p>
+                        <div className="class-container">
+                            
+                        </div>
+                        </div>
+                        <div className="cards-card">
+                        <i class="fas fa-heartbeat cardsIcons"></i>
+                        <p>Health<br />Insurance</p>
+                        <div className="class-container">
+                            
+                        </div>
+                        </div>
+                        <div className="cards-card">
+                        <i class="fas fa-car cardsIcons"></i>
+                        <p>Car<br />Insurance</p>
+                        <div className="class-container">
+                            
+                        </div>
+                        </div>
+                        <div className="cards-card">
+                        <i class="fas fa-motorcycle cardsIcons"></i>
+                        <p>2 Wheeler<br />Insurance</p>
+                        <div className="class-container">
+                            
+                        </div>
+                        </div>
+                        <div className="cards-card">
+                        <i class="fas fa-users cardsIcons"></i>
+                        <p>Family Health<br />Insurance</p>
+                        <div className="class-container">
+                            
+                        </div>
+                        </div>
+                        <div className="cards-card">
+                        <i class="fas fa-retweet cardsIcons"></i>
+                        <p>Guaranteed<br />return Plan</p>
+                        <div className="class-container">
+                            
+                        </div>
+                        </div>
+                        <div className="cards-card">
+                        <i class="fas fa-suitcase-rolling cardsIcons"></i>
+                        <p>Travel<br />Insurance</p>
+                        <div className="class-container">
+                            
+                        </div>
+                        </div>
+                        <div className="cards-card">
+                        <i class="fas fa-child cardsIcons"></i>
+                        <p>Child Saving<br />Plans</p>
+                        <div className="class-container">
+                            
+                        </div>
+                        </div>
+                        <div className="cards-card">
+                        <i class="fas fa-hourglass-half cardsIcons"></i>
+                        <p>Retirement<br />Plans</p>
+                        <div className="class-container">
+                            
+                        </div>
+                        </div>
+                        <div className="cards-card">
+                        <i class="fas fa-th-large cardsIcons"></i>
+                        <p>View All<br />Products</p>
+                        <div className="class-container">
+                            
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/landingPageComponent/ThirdSectionComponent.jsx
+++ src/components/landingPageComponent/ThirdSectionComponent.jsx
@@ -12,13 +12,14 @@ export default function ThirdSectionComponent() {
 
                     <div className="content">
                         <img src={twoWom} alt="twoWom" className="thirdSectionImg" />
+                        <hr className="" />
                         <div className="content-desc">
                             <h2><strong>When stuff happens, we’re there for you around the clock.</strong></h2>
                             <br />
-                            <h4>And with 19,000 agents across the country, feel free to  talk to an agent near your neighborhood.
+                            <h5>And with 19,000 agents across the country, feel free to  talk to an agent near your neighborhood.
                                 <br />
                                 We offer a wide range of products to help protect what you worked so hard to build.
-                            </h4>
+                            </h5>
                             <br />
                             <Link className="a-one" to="/insurance">View insurance</Link>
                         </div>
@@ -26,27 +27,27 @@ export default function ThirdSectionComponent() {
                     <br /><br />
                 </div>
             </div>
-            
+
             <div className="main-three">
                 <h1 className="headingTwo"><strong>Core differentiators that define our business</strong></h1>
-                <div className="imgs">
+                <div className="main-three-imgs">
                     <div className="core">
-                        <img src="https://sme.icicilombard.com/assetsSME/images/strong-brand-equitry.png" alt="1" />
+                        <i class="fas fa-dumbbell fa-3x"></i>
                         <h3>Strong <br /> Brand Equity</h3>
                     </div>
-                    <div className="core">
-                        <img src="https://sme.icicilombard.com/assetsSME/images/extensive-distribution-network.png" alt="2" />
+                    <div className="core"> 
+                        <i class="fas fa-suitcase fa-3x"></i>
                         <h3>Extensive <br />
                             Service Network
                         </h3>
                     </div>
                     <div className="core">
-                        <img src="https://sme.icicilombard.com/assetsSME/images/comprehensive-product-portfolio.png" alt="3" />
+                        <i class="fas fa-user-shield fa-3x"></i>
                         <h3>Comprehensive <br />
                             Product Portfolio</h3>
                     </div>
                     <div className="core">
-                        <img src="https://sme.icicilombard.com/assetsSME/images/technology-differentiation.png" alt="4" />
+                        <i class="fas fa-robot fa-3x"></i>
                         <h3>Technology <br />
                             Differentiation</h3>
                     </div>
