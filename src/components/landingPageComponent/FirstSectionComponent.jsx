e3e84822c34ef5ef0b037d8a8013616cbc03dcb7
gupta-shatakshi <guptashatakshi30@gmail.com>
2021-11-18 16:07:31 +0530

    changed cards using map and updated styling

--- src/components/landingPageComponent/FirstSectionComponent.jsx
+++ src/components/landingPageComponent/FirstSectionComponent.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import '../../styles/firstSection.css';
+import { Data } from './Data';
 
 export default function FirstSectionComponent() {
+
+    const itemData = Data.map((item)=>{
+        return(
+            <div className="cards-card">
+                <i class={item.class}></i>
+                <p>{item.p1}</p>
+                <p>{item.p2}</p>
+            </div>           
+        )
+    })
     return (
         <div className="firstSection-main">
             <div className="firstSection-main-one">
@@ -27,79 +38,8 @@ export default function FirstSectionComponent() {
                 </div>
             </div>
             <div className="firstSection-main-two">
-                <div className="firstSection-main-two-container">
-                    <div className="two-container-cards">
-                        <div className="cards-card">
-                        <i class="fas fa-umbrella cardsIcons"></i>
-                        <p>Term Life <br />Insurance</p>
-                        <div className="class-container">
-                            
-                        </div>
-                        </div>
-                        <div className="cards-card">
-                        <i class="fas fa-heartbeat cardsIcons"></i>
-                        <p>Health<br />Insurance</p>
-                        <div className="class-container">
-                            
-                        </div>
-                        </div>
-                        <div className="cards-card">
-                        <i class="fas fa-car cardsIcons"></i>
-                        <p>Car<br />Insurance</p>
-                        <div className="class-container">
-                            
-                        </div>
-                        </div>
-                        <div className="cards-card">
-                        <i class="fas fa-motorcycle cardsIcons"></i>
-                        <p>2 Wheeler<br />Insurance</p>
-                        <div className="class-container">
-                            
-                        </div>
-                        </div>
-                        <div className="cards-card">
-                        <i class="fas fa-users cardsIcons"></i>
-                        <p>Family Health<br />Insurance</p>
-                        <div className="class-container">
-                            
-                        </div>
-                        </div>
-                        <div className="cards-card">
-                        <i class="fas fa-retweet cardsIcons"></i>
-                        <p>Guaranteed<br />return Plan</p>
-                        <div className="class-container">
-                            
-                        </div>
-                        </div>
-                        <div className="cards-card">
-                        <i class="fas fa-suitcase-rolling cardsIcons"></i>
-                        <p>Travel<br />Insurance</p>
-                        <div className="class-container">
-                            
-                        </div>
-                        </div>
-                        <div className="cards-card">
-                        <i class="fas fa-child cardsIcons"></i>
-                        <p>Child Saving<br />Plans</p>
-                        <div className="class-container">
-                            
-                        </div>
-                        </div>
-                        <div className="cards-card">
-                        <i class="fas fa-hourglass-half cardsIcons"></i>
-                        <p>Retirement<br />Plans</p>
-                        <div className="class-container">
-                            
-                        </div>
-                        </div>
-                        <div className="cards-card">
-                        <i class="fas fa-th-large cardsIcons"></i>
-                        <p>View All<br />Products</p>
-                        <div className="class-container">
-                            
-                        </div>
-                        </div>
-                    </div>
+                <div className="two-container-cards">
+                        {itemData}
                 </div>
             </div>
         </div>
