9037c6249db29cd11becc481f64b26a429b857b1
kumarlava0007 <kumarlava0007@gmail.com>
2021-11-18 13:56:04 +0530

    login sign up forgot pass page change

--- src/components/userPage/ForgotPass.jsx
+++ src/components/userPage/ForgotPass.jsx
@@ -1,83 +1,29 @@
 import React from 'react'
-import style from "styled-components"
-import HandymanIcon from '@mui/icons-material/Handyman';
-const ForgotPass = () => {
+import Styles from '../../styles/login.module.css';
+import Style from '../../styles/forgotPass.module.css';
+import { Link } from 'react-router-dom';
+
+export default function forgotpassword() {
     return (
-        <div>
-            <HeadForgot>
-                <Image>
-                <img src="https://image.freepik.com/free-vector/cartoon-man-sitting-home-with-laptop_74855-6963.jpg" height="100%" width="750px"/>
-                </Image>
-                <SubHead>
-                <Icon>
-                    {/* <HandymanIcon style={{ color: "#4060BF",background:"whitesmoke",height:"135px",width:"141px",borderRadius:"70px 70px 60px 40px",padding:"20px"}}/> */}
-                    <i class="fas fa-tools"></i>
-                </Icon>
-                <InformationField>
-                    <FieldName>Enter Email</FieldName>
-                    <i class="fa fa-envelope-o fa-lg" aria-hidden="true" style={{color:"#4060BF"}}/><InputField type="email" placeholder="Enter Email"/>
-                    <FieldName>Set Password</FieldName>
-                    <i class="fa fa-key fa-lg" aria-hidden="true" style={{color:"red"}}/><InputField type="password" placeholder="Set Password"/><br/>
-                    <ForgotButton>
-                    <Button>Reset</Button>
-                    </ForgotButton>
-                </InformationField>
-                </SubHead>
-            </HeadForgot>
+        <div className={Styles.container}>
+            <div className={Styles.sectionOne}>
+                <div className={Style.icon}>
+                <i class="fas fa-tools"></i>
+                </div>
+                <h3 style={{textAlign: "center"}}><strong>Reset Your Password</strong></h3>
+            </div>
+            <div className={Styles.sectionOne}>
+                <div className={Styles.formContainer}>
+                    <form className={Styles.formTab}>
+                        <input type="email" id="username" required placeholder="Email address or phone number" className={Styles.formInput}/>
+                        <input type="text" id="verify" required placeholder="Code" className={Styles.formInput} />
+                        <input type="password" id="password" required placeholder="Password" className={Styles.formInput} />
+                        <input type="password" id="Cpassword" required placeholder="Confirm Password" className={Styles.formInput} />
+                        <hr className={Styles.hr}/>
+                        <Link to="/LogIn"><button className={Styles.buttonTwo}>Reset</button></Link>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
-
-export default ForgotPass
-const HeadForgot=style.div`
-margin-top:100px;
-display:flex;
-justify-content:space-around;
-`
-const Image=style.div`
-
-`
-const SubHead=style.div`
-
-`
-const Icon=style.div`
-display:flex;
-justify-content:center;
-`
-const InformationField=style.div`
-margin-top:-60px;
-box-shadow:7px 7px 8px rgba(100, 100, 100, 0.5);
-background-color:whitesmoke;
-border-radius:10px;
-padding:60px 50px 30px 50px;
-`
-const FieldName=style.h4`
-margin-top:20px;
-margin-bottom:10px;
-text-align:center;
-`
-const InputField=style.input`
-width:400px;
-height:40px;
-font-size:20px;
-border-style:none;
-background-color:whitesmoke;
-outline:none;
-border-bottom:1px solid #4060BF;
-text-indent:10px;
-`
-const ForgotButton=style.div`
-display:flex;
-justify-content:center;
-`
-const Button=style.div`
-margin-top:20px;
-padding:3px 70px 3px 70px;
-font-weight:bolder;
-background-color:#BE4871;
-color:white;
-cursor:pointer;
-border:2px solid #BE4871;
-border-radius:5px;
-font-size:20px;
-`
