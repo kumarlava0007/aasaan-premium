34d19e6ef28c1a1e936d8543dcdeef734dff21cd
gupta-shatakshi <guptashatakshi30@gmail.com>
2021-11-25 16:15:44 +0530

    completed insurance page

--- src/components/navbar pages/Insurance.jsx
+++ src/components/navbar pages/Insurance.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from 'react';
+import classes from '../../styles/insurance.module.css';
+import {InsuranceData} from './InsuranceData';
 
 const Insurance = () => {
+    const cards = InsuranceData.map((item)=>{
+        return(
+            <div className={classes.cards}>
+                <div className={classes.cards1}>
+                    <i class={item.class}></i>
+                    <p>{item.p1}</p>
+                </div>
+                <div className={classes.cards2}>
+                    <p>{item.p2}</p>
+                    <button className={classes.buyBtn}>Buy Plan</button>
+                </div>
+            </div>           
+        )
+    })
     return (
-        <>
-        <br /><br />
-        <br /><br />
-            Insurance page
-        </>
+        <div className={classes.container}>
+            {/* <br />
+            <br />
+            <br />
+            <br /> */}
+            <h2>Get Insured easily with us</h2>  
+            <div className={classes.subContainer}>
+                {cards}
+            </div>
+        </div>
     )
 }
 
