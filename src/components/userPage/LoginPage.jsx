59288ca6487ba2de2f9cd4219230cbe47440b557
gupta-shatakshi <guptashatakshi30@gmail.com>
2021-12-01 11:20:21 +0530

    added sign in with google using Oauth 2.0 but still want some help with that

--- src/components/userPage/LoginPage.jsx
+++ src/components/userPage/LoginPage.jsx
@@ -1,8 +1,25 @@
+/* global gapi */
 import React from 'react'
 import Styles from '../../styles/login.module.css';
 import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
+
+        const onSignIn = (googleUser) => {
+          var profile = googleUser.getBasicProfile();
+          console.log('ID: ' + profile.getId()); 
+          console.log('Name: ' + profile.getName());
+          console.log('Image URL: ' + profile.getImageUrl());
+          console.log('Email: ' + profile.getEmail());
+          }
+      
+      
+        //   const signOut = () => {
+        //     var auth2 = gapi.auth2.getAuthInstance();
+        //     auth2.signOut().then(function () {
+        //     console.log('User signed out.');
+        //     });
+        // }
     return (
         <div className={Styles.container}>
             <div className={Styles.sectionOne}>
@@ -16,6 +33,10 @@ export default function LoginPage() {
                         <input type="password" id="password" required placeholder="Password" className={Styles.formInput} />
                         <button className={Styles.buttonOne} type="submit">Log In</button>
                         <Link className={Styles.forgPass} to="/forgotpassword">Forgotten Password</Link>
+                        <div className="g-signin2"
+                        //  className={Styles.signInWithGoogle} 
+                         data-onsuccess={onSignIn}/><br />
+                        {/* <a href="#/login" onClick={signOut}>Sign out</a> */}
                         <hr className={Styles.hr}/>
                         <Link to="/signUp"><button className={Styles.buttonTwo}>Create New Account</button></Link>
                     </form>
