fcd720ecd9fd1f9d34cd8efccce3c0d743e07303
gupta-shatakshi <guptashatakshi30@gmail.com>
2021-12-23 12:54:04 +0530

    changed icons in landing page

--- src/components/landingPageComponent/Data.jsx
+++ src/components/landingPageComponent/Data.jsx
@@ -1,23 +1,23 @@
 export const Data = [{
-    class: "fas fa-th-large cardsIcons",
+    class: "fas fa-users",
     p1: "Business ",
     p2: "Insurance",
 },{
-    class: "fas fa-th-large cardsIcons",
+    class: "fas fa-user",
     p1: "Individual",
     p2: "Insurance",
 },{
-    class: "fas fa-th-large cardsIcons",
-    p1: "View all",
-    p2: "Products",
+    class: "fas fa-home",
+    p1: "Home",
+    p2: "Insurance",
 },{
-    class: "fas fa-th-large cardsIcons",
-    p1: "View all",
-    p2: "Products",
+    class: "fas fa-car-crash",
+    p1: "Automobile",
+    p2: "Insurance",
 },{
-    class: "fas fa-th-large cardsIcons",
-    p1: "View all",
-    p2: "Products",
+    class: "fas fa-laptop",
+    p1: "Gadgets ",
+    p2: "Insurance",
 },{
     class: "fas fa-th-large cardsIcons",
     p1: "View all",
